f3262857338b5f576d5383b45e1ba0b5cf96d3af
Jonas Walkling <jonas@walkling.email>
2023-08-28 16:38:00 +0200

    Add exports

--- lib/dart_s3_upload.dart
+++ lib/dart_s3_upload.dart
@@ -4,3 +4,5 @@ import 'dart:core';
 
 export 'package:dart_s3_upload/src/content.dart';
 export 'package:dart_s3_upload/src/client.dart';
+export 'package:dart_s3_upload/src/client_response.dart';
+export 'package:optionals/optionals.dart';
